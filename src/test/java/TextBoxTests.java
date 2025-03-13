4c7130ee41cc3d1f9ec82a03efec18adfdc85b21
Pavel B <war4040@yandex.ru>
2025-03-13 23:10:09 +0400

    Test Demoqa UI update

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,9 +1,12 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -18,6 +21,8 @@ public class TextBoxTests {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+
 
     }
 
@@ -26,24 +31,30 @@ public class TextBoxTests {
         open("/automation-practice-form");
 
         $("#firstName").setValue("Alex Egorov");
+        $("#firstName").setValue("Alex Egorov");
         $("#lastName").setValue("Lovashkin");
-        $("#userEmail").setValue("alex@egorov.com");
+        $("#userEmail").setValue("alex@egoro v.com");
         $("[for=gender-radio-1]").click();
         $("#userNumber").setValue("8800355353");
         $("#dateOfBirthInput").click();
-   $(byText("29")).click();
-    $("#subjectsInput").sendKeys("C");
+       $(".react-datepicker__month-select").selectOption("March");
+       $(".react-datepicker__year-select").selectOption("2025");
+       $(".react-datepicker__day--013").click();
+       $("#subjectsInput").setValue("Commerce").pressEnter();
+       $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/img/123.png"));
+        $("#currentAddress").setValue("Some address 1");
 
-     sleep(5000);
+        sleep(5000);
 
-     //   $("#permanentAddress").setValue("Another address 1");
-       // $("#submit").click();
+        //   $("#permanentAddress").setValue("Another address 1");
+        // $("#submit").click();
 
         //$("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.com"),
-          //      text("Some address 1"), text("Another address 1"));
+        //      text("Some address 1"), text("Another address 1"));
 
     }
 
 
-    }
+}
 
